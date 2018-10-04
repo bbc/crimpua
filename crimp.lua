7edcdc4bc359dc4169389965520dbd913f98a411
Ryan Wood <Ryan.Wood01@bbc.co.uk>
2018-10-04 12:54:18 +0100

    Pass the nil case and more number cases

--- crimp.lua
+++ crimp.lua
@@ -1,11 +1,11 @@
 Crimpua = Crimpua or {}
 
 function Crimpua.notation (data)
-   if type(data) == "string"  then return(data .. "S") end
---   if type(data) == "number"  then return(data .. "N") end -- FIXME : make compatible with Lua 5.1
---   if type(data) == "boolean" then return(tostring(data) .. "B") end
---   if type(data) == "nil"     then return("_") end
---   if type(data) == "table"   then return(Crimpua.coll(data)) end
+    if type(data) == "nil"     then return("_") end
+    if type(data) == "string"  then return(data .. "S") end
+    if type(data) == "number"  then return(data .. "N") end
+    --   if type(data) == "boolean" then return(tostring(data) .. "B") end
+    --   if type(data) == "table"   then return(Crimpua.coll(data)) end
 end
 
 return Crimpua
