a932f449bd74ad483a21574123398beb4debe4e5
Ryan Wood <Ryan.Wood01@bbc.co.uk>
2018-10-04 14:50:58 +0100

    Passing flat array structure case

--- crimp.lua
+++ crimp.lua
@@ -1,11 +1,28 @@
 Crimpua = Crimpua or {}
 
+local function map(func, array)
+    local new_array = {}
+    for i,v in ipairs(array) do
+        new_array[i] = func(v)
+    end
+    return new_array
+end
+
+local function stringCompare(a, b)
+    return tostring(a) < tostring(b)
+end
+
+local function notateCollection(data)
+    table.sort(data, stringCompare)
+    return map(Crimpua.notation,data)
+end
+
 function Crimpua.notation (data)
     if type(data) == "nil"     then return("_") end
     if type(data) == "string"  then return(data .. "S") end
     if type(data) == "number"  then return(data .. "N") end
     if type(data) == "boolean" then return(tostring(data) .. "B") end
-    --   if type(data) == "table"   then return(Crimpua.coll(data)) end
+    if type(data) == "table"   then return(table.concat(notateCollection(data)) .. "A") end
 end
 
 return Crimpua
--- crimp_spec.lua
+++ crimp_spec.lua
@@ -37,4 +37,23 @@ describe("The notation strings", function()
             assert.is_equal("1N3NaSA", Crimpua.notation({1, "a", 3}))
         end)
     end)
+--    # Lua tables make no distinction between a table value being nil and the corresponding key not existing in the table
+--    # this makes this case unecessary, and not easily matchable in lua
+--    # although some users use null = {} and check for null rather than nil to handle this
+--    describe("Given an array containing nil", function()
+--        it("returns all the notated values with an A suffix", function()
+--            assert.is_equal("_1N3NaSA", Crimpua.notation({3, nil, 1, "1"}))
+--        end)
+--    end)
+    describe("Given an array containing capital letters", function()
+        it("returns all the notated values with an A suffix", function()
+            assert.is_equal("ASBSaSbSA", Crimpua.notation({"a", "A", "b", "B"}))
+        end)
+    end)
+    describe("Given an array containing a nested array", function()
+        it("returns all the notated values with an A suffix", function()
+            assert.is_equal("1N2SbSAaSA", Crimpua.notation({"a", 1, {"b", "2"}}))
+        end)
+    end)
+
 end)
