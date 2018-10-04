6d0d8e3de84ce9b8b7e76800d9f5edfd1c45be73
Ryan Wood <Ryan.Wood01@bbc.co.uk>
2018-10-04 13:05:06 +0100

    Pass the boolean case

--- crimp.lua
+++ crimp.lua
@@ -4,7 +4,7 @@ function Crimpua.notation (data)
     if type(data) == "nil"     then return("_") end
     if type(data) == "string"  then return(data .. "S") end
     if type(data) == "number"  then return(data .. "N") end
-    --   if type(data) == "boolean" then return(tostring(data) .. "B") end
+    if type(data) == "boolean" then return(tostring(data) .. "B") end
     --   if type(data) == "table"   then return(Crimpua.coll(data)) end
 end
 
