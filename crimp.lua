2237af1b046d24b32b0dacadf0ed8e9fae01ce5f
Ryan Wood <Ryan.Wood01@bbc.co.uk>
2018-10-04 12:49:48 +0100

    Pass string case

--- crimp.lua
+++ crimp.lua
@@ -1,7 +1,7 @@
 Crimpua = Crimpua or {}
 
 function Crimpua.notation (data)
---   if type(data) == "string"  then return(data .. "S") end
+   if type(data) == "string"  then return(data .. "S") end
 --   if type(data) == "number"  then return(data .. "N") end -- FIXME : make compatible with Lua 5.1
 --   if type(data) == "boolean" then return(tostring(data) .. "B") end
 --   if type(data) == "nil"     then return("_") end
